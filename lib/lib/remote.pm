6e4147dd4bbbbdd71f58ff9525177db0c3e6f27f
Михаил Че <mche@cpan.org>
2013-06-24 12:03:39 +0000

    Import of MCHE/lib-remote-0.10 from CPAN.
    gitpan-cpan-distribution: lib-remote
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         MCHE/lib-remote-0.10.tar.gz
    gitpan-cpan-author:       MCHE
    gitpan-cpan-maturity:     released

--- lib/lib/remote.pm
+++ lib/lib/remote.pm
@@ -4,7 +4,160 @@ use 5.006;
 use strict;
 use warnings;
 use LWP::UserAgent;
+#~ use Data::Dumper;
 #~ use Carp qw(croak carp);
+
+=head1 VERSION
+
+Version 0.10
+
+=cut
+
+our $VERSION = '0.10';
+
+my $ua = LWP::UserAgent->new;
+my $url_re = qr#^(https?|ftp|file)://#i;
+#~ $ua->timeout(10);
+my %config = (#сохранение списка пар "Имя::модуля"=>{url => ..., ..., ...}
+    __PACKAGE__ => {require=>1, cache=>1, debug=>0},
+    __urls__ => [],# сохранение общих путей 
+    __content__ => {},# cache
+);
+
+#~ my $pkg = 'lib::remote';
+
+BEGIN {
+    push @INC, sub {# диспетчер
+        my $self = shift;# эта функция CODE(0xf4d728) вроде не нужна
+        my $arg = shift;#Имя/Модуля.pm
+        
+        my $path = my $mod = $arg;
+        $mod =~ s|/+|::|g;
+        $mod =~ s|\.pm$||g;
+        $path =~ s|::|/|g;
+        $path =~ s|\.pm$||;
+        $path .= '.pm';
+        
+        my $conf = $config{$mod};
+        my $debug = ($conf && $conf->{debug}) // $config{__PACKAGE__}{debug};
+        my $cache = ($conf && $conf->{cache}) // $config{__PACKAGE__}{cache};
+        
+        warn __PACKAGE__, ": try dispatch of [$mod][path=$path][arg=$arg]]" if $debug;
+        
+        if ($cache && $config{__content__}{$mod}) {
+            warn __PACKAGE__, ": get cached content of [$mod]" if $debug;
+            open my $fh, '<', \$config{__content__}{$mod} or die "Cant open: $!";
+            return $fh;
+        }
+
+        my $content;
+        
+        if ($conf && $conf->{url} && $conf->{url} =~ /$url_re/) {# конкретный модуль
+            #~ warn "Неверный или кривой урл[$m->{url}] для модуля [$mod]"
+                #~ and return undef
+                #~ unless $m->{url} && $m->{url} =~ /$url_re/;
+            $content = _lwpget($conf->{url});
+            warn __PACKAGE__, ": couldn't get content the module [$mod] by link [$conf->{url}]" if $debug && !$content;
+        }
+        unless ($content) {# перебор удаленных папок
+            
+            for (@{$config{__urls__}}) {
+                s|/$||;
+                warn __PACKAGE__, ": try get [$_/$path] for [$mod]" if $debug;#": get [$mod] content";
+                $content = _lwpget("$_/$path");
+                if ($content) {
+                    warn __PACKAGE__, ": success LWP get content [$mod] by link [$_/$path]" if $debug;#": get [$mod] content";
+                    last;
+                }
+            }
+        }
+        
+        return undef unless $content;
+        
+        $config{__content__}{$mod} = $content if $cache ;
+
+        open my $fh, '<', \$content or die "Cant open: $!";
+        return $fh;
+    };
+}
+
+sub import { # это разбор аргументов после строк use lib::remote ...
+    my $pkg = shift;# is eq __PACKAGE__
+    $pkg->config(@_);
+    my $last_modules = delete $config{__PACKAGE__}{_last_modules}
+        or return;
+    
+    my $module;
+    for my $module (@$last_modules) {
+        my $require = $config{$module}{require} // $config{__PACKAGE__}{require};
+        my $debug = $config{$module}{debug} // $config{__PACKAGE__}{debug};
+        if ( $require ) {
+            #~ eval "use $module;";# вот сразу заход в диспетчер @INC
+            eval {require $module};
+            if ($@) {
+                warn __PACKAGE__, ": возможно проблемы с модулем [$module]: $@";
+            } elsif ($debug) {
+                warn __PACKAGE__, ": success done [require $module]\n"  if $debug;
+            }
+        }
+        my $import = $config{$module}{import};# || $config{__PACKAGE__}{import};
+        
+        if ($require && $import && @$import) {
+            eval {$module->import(@$import)};
+            if ($@) {
+                warn __PACKAGE__, ": возможно проблемы с импортом [$module]: $@";
+            } else {
+                warn __PACKAGE__, ": success done [$module->import(@{[@$import]})]\n" if $debug;
+            }
+        }
+    }
+}
+
+sub config {
+    my $pkg = shift;# is eq __PACKAGE__
+    my $module;
+    for my $arg (@_) {
+        my $opt = _opt($arg);
+        if ($module) {
+            if ( $module eq '__PACKAGE__' ) {
+                my $url = delete $opt->{url};
+                push @{$config{__urls__}}, $url if $url && $url =~ /$url_re/ && !($url ~~ @{$config{__urls__}});
+            } else {
+                push @{$config{__PACKAGE__}{_last_modules}}, $module;
+            }
+            @{$config{$module}}{keys %$opt} = values %$opt;
+            $module = undef; # done pair
+        } elsif ($opt->{url} && $opt->{url} =~ /$url_re/) {
+            push @{$config{__urls__}}, $opt->{url} unless $opt->{url} ~~ @{$config{__urls__}};#$unique{$arg}++;
+        } elsif (!ref($arg)) {
+            $module = $arg;
+        } else {
+            @{$config{__PACKAGE__}}{keys %$opt} = values %$opt;
+        }
+    }
+    return \%config;
+}
+
+sub _opt {
+    my $arg  = shift;
+    my $ret = {url=>$arg,} unless ref($arg);
+    $ret ||= {$arg->[0] =~ /$url_re/ ? (url=>@$arg) : @$arg,} if ref($arg) eq 'ARRAY';
+    $ret ||= $arg if ref($arg) eq 'HASH';
+    return $ret;
+}
+
+sub _lwpget {
+    my $url = shift;
+    #~ print "get: $self";
+    my $get = $ua->get($url);
+    if ( $get->is_success ) {
+        #~ print "lwpget success [$url]\n";
+        return $get->decoded_content();# ??? ->content нужно отладить charset=>'cp-1251'
+    } else {
+        #~ die "LWP::UserAgent->get($url) failed: ". $module->status_line."\n";
+        return undef;
+    }
+}
 
 =encoding utf8
 
@@ -14,7 +167,6 @@ use LWP::UserAgent;
 
 Hello all! Nice health! Good thinks!
 
-=cut
 
 =head1 NAME
 
@@ -26,13 +178,7 @@ lib::remote - pragma for use remote modules without installation basically throu
 
 Кто-то еще стырил L</http://www.linuxdigest.org/2012/06/use-modules-on-remote-servers/> (поздняя дата и есть ошибки)
 
-=head1 VERSION
 
-Version 0.05
-
-=cut
-
-our $VERSION = '0.05';
 
 
 =head1 FAQ
@@ -41,9 +187,13 @@ Q: Зачем? Why?
 
 A: За лосем. For elk.
 
-Q: Почему? Why?
+Q: Почему? And why?
 
 A: По кочану. For head of cabbage.
+
+Q: Как?
+
+A: Да вот так.
 
 
 =head1 SYNOPSIS
@@ -65,29 +215,30 @@ A: По кочану. For head of cabbage.
 Тогда делаем пары ключ->значение, указывая КОНКРЕТНЫЙ урл для КОНКРЕТНОГО модуля, например:
 
     use lib::remote
-        'http://....',
         'Some::Module1'=>'https://<хост>/.../?key=ede35ac1208bbf479&...',
         'SomeModule2'=>'ssh://user:pass@host:/..../SomeModule2.pm',
     ;
-    #use Some::Module1; не нужно, уже сделано (см. L</"Конфигурация модуля">)
-    use SomeModule2 qw(func1 func2);# только, если нужно что-то импортировать, простое use уже сделано (см. L</"Конфигурация модуля">)
-    use parent 'Some::Module1';
+    #use Some::Module1; не нужно, уже сделано require (см. "Опцию [require] расширенного синтаксиса")
+    use SomeModule2 qw(func1 func2), [<what ever>, ...];# только, если нужно что-то импортировать очень сложное (см. "Опцию [import] расширенного синтаксиса")
+    use parent 'Some::Module1'; # такое нужно
     ...
 
-Не трудно догадаться, что вычленение пар в общем списке происходит по специфике URI.
 
 B<Внимание>
 
-Конкретно указанный модуль (через пару) будет искаться сначала в своем урл, а потом в безымянных безключевых параметрах.
+Конкретно указанный модуль (через пару) будет искаться сначала в своем урл, а потом во всех заданных урлах глобального конфига.
 
 При многократном вызове use lib::remote все параметры и урлы сохраняются, аналогично use lib '';, но естественно не в @INC. Повторюсь, в @INC помещается только один диспетчер.
 
 =head2 Расширенный синтаксис
 
     use lib::remote
+        # global config for modules unless them have its own
         'http://....',
         ['http://....', opt1 =>..., opt2 =>..., ....],
         {url=>'http://....', opt1 =>..., opt2 =>..., ....},
+
+    # per module personal options
         'Some::Module1'=> 'http://....',
         'Some::Module2'=>['http://...', opt1 =>..., opt2 =>..., ....],
         'Some::Module3'=>{url => 'http://...', opt1 =>..., opt2 =>..., ....},
@@ -97,38 +248,31 @@ B<Внимание>
     ...
 
 
+Не трудно догадаться, что вычленение пар в общем списке происходит по специфике URI.
+
 Опции:
 
 =over 4
 
-=item * charset => 'utf-8', Задать кодировку урла. Если веб-сервер правильно выдает C<Content-Type: application/x-perl; charset=...>, тогда не нужно, ->decoded_content сработает. Помнить про C<use utf8;>
+=item * url => '>schema://>...' Это основной параметр. На уровне глобальной конфигурации сохраняется список всех урлов, к которым добавляется путь Some/Module.pm
+
+=item * charset => 'utf8', Задать кодировку урла. Если веб-сервер правильно выдает C<Content-Type: ...; charset=utf8>, тогда не нужно, ->decoded_content сработает. Помнить про C<use utf8;>
+
+=item * require => 1|0 Cрабатывает require Some::Module1; Поэтому не нужно делать строку use|require Some::Module;, если только нет хитрых импортов (см. опцию import ниже)
+
+=item * import => [qw(), ...]. The import spec for loaded module. Disadvantage!!! Work on list of scalars only!!! Просто вызывается Some::Module1->import(...);
+
+=item * cache => 1|0 Content would be cached
+
+=item * debug => 0|1 warn messages
 
 =item * что еще?
 
 =back
 
-=head2 Конфигурация модуля lib::remote
 
-Просто передаем дополнительную пару ключ->значение:
+Можно многократно вызывать use lib::remote ...; и тем самым изменять настройки модулей и глобальные опции.
 
-    use lib::remote
-        'lib::remote'=>[opt1 =>..., opt2 =>..., ....],
-        or 'lib::remote'=>{opt1 =>..., opt2 =>..., ....},
-        ...
-    ;
-    ...
-
-Видно, что ключ должен совпадать с именем этого модуля 'lib::remote'. Это и есть признак конфигурационных данных.
-
-Основные опции:
-
-=over 4
-
-=item * autouse => 0. По умолчанию 1, т.е. для конкретных модулей 'Some::Module1'=> .... срабатывает use Some::Module1;
-
-=item * что еще?
-
-=back
 
 =head1 Требования REQUIRES
 
@@ -165,102 +309,9 @@ Url может возвращать сразу пачку модулей (packag
     ...
 
 
-=cut
-
-
-
-my %config = (
-    autouse =>1,
-);
-my $ua = LWP::UserAgent->new;
-my $url_re = qr#^(https?|ftp|file)://#i;
-#~ $ua->timeout(10);
-my %modules = ();#сохранение списка пар "Имя::модуля"=>{url => ..., ..., ...}
-my @base_ulrs = ();# сохранение общих путей {url=>..., ..., ...}
-
-BEGIN {
-    push @INC, sub {# диспетчер
-        my $self = shift;# эта функция CODE(0xf4d728) вроде не нужна
-        my $arg = shift;#Имя/Модуля.pm
-        my $mod = $arg;
-        $mod =~ s#/#::#g;
-        $mod =~ s#\.pm$##g;
-        #~ print "remote INC sub: module=[$mod]\n",;# lwpget: arg=[Имя/Модуля.pm] Имя::Модуля
-        #~ return undef unless $url;
-        my $content;
-        if (my $m = $modules{$mod}) {# конкретный модуль
-            $content = lwpget($m->{url});
-        }
-        unless ($content) {# перебор удаленных папок
-            for (@base_ulrs) {
-                $content = lwpget("$_->{url}/$arg");
-                last if $content;
-            }
-        }
-        return undef unless $content;
-        
-        open my $fh, '<', \$content or die "Cant open: $!";
-        return $fh;
-    };
-}
-
-sub import { # это разбор аргументов после строк use lib::remote ...
-    my $pkg = shift;# is eq __PACKAGE__
-    my $module;
-    for my $arg (@_) {
-        my $opt = _opt($arg);
-        if ($module) {
-            if ( $module eq __PACKAGE__ ) {
-                $arg = {@$arg} if ref($arg) eq 'ARRAY';
-                @config{keys %$arg} = values %$arg;
-                #~ print "config:", %config, "\n";
-            } else {
-                if ($opt->{url} =~ /$url_re/) {
-                    $modules{$module} = $opt;
-                    if ($modules{$module}{autouse} || $config{autouse} ) {
-                        #~ eval "use $module;";# вот сразу заход в диспетчер @INC
-                        eval {require $module};
-                        warn "Возможно проблемы с модулем [$module]: $@" if $@;
-                    }
-                } else {
-                    warn "Bad or no url[$opt->{url}] for [$module]";
-                }
-            }
-            $module = undef;
-            next;
-        } elsif ($opt->{url} =~ /$url_re/) {
-            push @base_ulrs, $opt unless $opt->{url} ~~ [map {$_->{url}} @base_ulrs];#$unique{$arg}++;
-        } else {
-            $module = $arg;
-        }
-    }
-}
-
-sub _opt {
-    my $arg  = shift;
-    my $ret = {url=>$arg,} unless ref($arg);
-    $ret ||= {url=>@$arg,} if ref($arg) eq 'ARRAY';
-    $ret ||= $arg if ref($arg) eq 'HASH';
-    $ret->{url} =~ s#/$## if $ret->{url};
-    return $ret;
-}
-
-sub lwpget {
-    my $url = shift;
-    #~ print "get: $self";
-    my $get = $ua->get($url);
-    if ( $get->is_success ) {
-        #~ print "lwpget success [$url]\n";
-        return $get->decoded_content();# ??? ->content нужно отладить charset=>'cp-1251'
-    } else {
-        #~ die "LWP::UserAgent->get($url) failed: ". $module->status_line."\n";
-        return undef;
-    }
-}
-
 =head1 AUTHOR
 
-Mikhail Che, C<< <m.che[пёсик]aukama.dyndns.org> >>
+Mikhail Che, C<< <m[пёсик]cpan.org> >>
 
 =head1 BUGS
 
@@ -313,15 +364,27 @@ L<http://search.cpan.org/dist/lib-remote/>
 
 Глянь L<lib::http>
 
+Глянь L<lib::dbi>
+
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2012 Mikhail Che.
+Copyright 2012-2013 Mikhail Che.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
 by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
+
+=head1 DISTRIB
+
+$ module-starter --module=lib::remote --author=”Mikhail Che” --email=”m.che@cpan.org" --builder=Module::Build --license=perl --verbose
+
+$ perl Build.PL
+
+$ ./Build test
+
+$ ./Build dist
 
 
 =cut
--- t/01-use.t
+++ t/01-use.t
@@ -1,33 +1,97 @@
 #!perl -T
 
-use Test::More tests => 2;
+use Data::Dumper;
+
+use Test::More tests => 1;
 
 #BEGIN {
 #    use_ok( 'lib::remote' ) || print "Bail out!\n";
 #}
 
+#~ =pod
 use lib::remote
-	'lib::remote'=> {autouse=>0},
-	#'Module::Stubber'=>'http://api.metacpan.org/source/MNUNBERG/Module-Stubber-0.03/lib/Module/Stubber.pm',
-	'http://api.metacpan.org/source/MNUNBERG/Module-Stubber-0.03/lib/',
+	#~ 'http://api.metacpan.org/source/MNUNBERG/Module-Stubber-0.03/lib/',
+	'Module::Stubber'=>{
+		url=>'http://api.metacpan.org/source/MNUNBERG/Module-Stubber-0.03/lib/Module/Stubber.pm',
+		#~ import=>[[qw(some symbols),],  ],
+		import=>[qw(Unavailable::Module::As::Stub), [qw(some symbols)], ],
+		debug=>0,
+	},
+	
+	;
+my $stub = Unavailable::Module::As::Stub->new;
+use Module::Stubber 'Unavailable::Module::As::Stub2' => [qw(some symbols)], 'silent'=>1,;#qw(some symbols)
+my $stub2 = Unavailable::Module::As::Stub2->new;
+#~ some();
+
+use lib::remote
+	'http://api.metacpan.org/source/TOBYINK/Module-Hash-0.001/lib',
+	{debug=>1,},
+	'Module::Hash'=>{
+		#~ url=>'http://api.metacpan.org/source/TOBYINK/Module-Hash-0.001/lib/Module/Hash.pm', 
+		#~ import=>[{prefix => "Math"}],
+		debug=>0,
+	};
 	;
 
-use Module::Stubber 'Some::Unavailable::Module::As::Stub' => [], 'silent'=>1,;#qw(some symbols)
-my $stub = Some::Unavailable::Module::As::Stub->new;
+my %MOD;
+use Module::Hash \%MOD; # OK
+#~ tie %MOD, "Module::Hash"; # OK
+my $num2 = 
+	#~ $MOD2->{"BigInt"}->new(24_000_000_000_000_000)->bsqrt()
+	#~ + 
+	$MOD{"Math::BigInt"}->new(42_000_000_000_000_001)->blog()
+;
 
-ok($stub, 'Test Module::Stubber');
-diag( "Testing lib::remote Module::Stubber[$Module::Stubber::VERSION] [$stub]" );
 
 
+ok($stub && $stub2 && $num2, 'Test failed');
+diag( "Testing lib::remote Module::Stubber($Module::Stubber::VERSION)=[$stub]&[$stub2] Module::Hash($Module::Hash::VERSION)::Math::BigInt=[$num2]\n",  );#Dumper(\%INC)
 
-use lib::remote 'Module::Hash'=>['http://api.metacpan.org/source/TOBYINK/Module-Hash-0.001/lib/Module/Hash.pm', autouse=>1];
-#use Module::Hash; dont need 
+#~ __END__
+#~ =cut
+
+=pod
+use lib::remote
+	'http://api.metacpan.org/source/TOBYINK/Module-Hash-0.001/lib',
+	'Module::Hash'=>{
+		#~ url=>'http://api.metacpan.org/source/TOBYINK/Module-Hash-0.001/lib/Module/Hash000.pm', 
+		#~ require=>0,
+	};
+#~ use Module::Hash; #dont need 
  
 tie my %MOD, "Module::Hash";
-my $num = $MOD{"Math::BigInt"}->new(42_000_000_000_000_000);
+#~ warn $MOD;
+my $num = $MOD{"Math::BigInt"}->new(42_000_000_000_000_000)->bsqrt();
 
 ok($num, 'Test Module::Hash');
 
-diag( "Testing lib::remote Module::Hash [$Module::Hash::VERSION] Math::BigInt::[$num]" );
+diag( "Testing lib::remote Module::Hash [$Module::Hash::VERSION] Math::BigInt::bsqrt(42_000_000_000_000_000)=[$num]\n", );# Dumper(\%INC)
 
+=cut
 
+=pod
+#~ my $MOD = my $MOD2 = {};
+use lib::remote
+	'http://api.metacpan.org/source/TOBYINK/Module-Hash-0.001/lib',
+	'Module::Hash'=>{
+		#~ url=>'http://api.metacpan.org/source/TOBYINK/Module-Hash-0.001/lib/Module/Hash.pm', 
+		#~ import=>[$MOD],
+	};
+	;
+#~ use Module::Hash;
+#~ require Module::Hash;
+my $MOD2;
+use Module::Hash $MOD2;
+tie my %MOD, "Module::Hash";
+#~ 'Module::Hash'->import($MOD, $MOD2);
+#~ warn $MOD;
+my $num2 = 
+	$MOD2->{"Math::BigInt"}->new(24_000_000_000_000_000)->bsqrt()
+	+ 
+	$MOD{"Math::BigInt"}->new(42_000_000_000_000_001)->blog()
+;
+
+ok($num2, 'Test Module::Hash');
+diag( "Testing lib::remote Module::Hash [$Module::Hash::VERSION] Math::BigInt=[$num2]\n", );# Dumper(\%INC)
+=cut
